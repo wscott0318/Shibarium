a6c79ca56a51f5eb4e338f795cb77a29241e5447
rupesh.verma <rupeshkhewar@gmail.com>
2022-07-04 10:58:50 +0530

    all validator list bugs

--- src/pages/all-validator/gridView/index.tsx
+++ src/pages/all-validator/gridView/index.tsx
@@ -12,45 +12,51 @@ export default function ValidatorGrid({ validatorsList }: { validatorsList: any 
             <div className='ValidatorGrid-sec'>
                 <div className='container'>
                     <div className='grid-sec'>
-                        <div className='row side-cover'>
-                            {validatorsList.map((validator: any) => {
-                                return (
-                                    <div className='col-xl-3 col-sm-6 col-12 side-space'>
-                                        <div className='box'>
-                                            <div className='box-head'>
-                                                <div className='d-flex align-items-center justify-content-start'>
-                                                    <div>
-                                                        <span className='user-icon'></span>
+                        {validatorsList && validatorsList.length ?
+                            <div className='row side-cover'>
+                                {validatorsList.map((validator: any) => {
+                                    return (
+                                        <>
+                                            <div className='col-xl-3 col-sm-6 col-12 side-space'>
+                                                <div className='box'>
+                                                    <div className='box-head'>
+                                                        <div className='d-flex align-items-center justify-content-start'>
+                                                            <div>
+                                                                <span className='user-icon'></span>
+                                                            </div>
+                                                            <div className='fw-700'>
+                                                                <span className='vertical-align'>{validator.name}</span>
+                                                                <p><span className='ft-16 light-text'>{(+validator.stakeAmount).toFixed(8)} BONE Staked</span></p>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                    <div className='fw-700'>
-                                                        <span className='vertical-align'>{validator.name}</span>
-                                                        <p><span className='ft-16 light-text'>{(+validator.stakeAmount).toFixed(8)} BONE Staked</span></p>
+                                                    <div className='box-body'>
+                                                        <div className='d-flex align-items-center justify-content-between'>
+                                                            <div className='fw-600 ft-16'>Performance</div>
+                                                            <div>
+                                                                <span className='warning-color fw-600 ft-14'>100%</span>
+                                                            </div>
+                                                        </div>
+                                                        <div className='d-flex align-items-center justify-content-between'>
+                                                            <div className='fw-600 ft-16'>Commission</div>
+                                                            <div>
+                                                                <span className='warning-color fw-600 ft-14'>{validator.commissionRate}%</span>
+                                                            </div>
+                                                        </div>
+                                                        <div className='mt-3 text-center'>
+                                                            <button type="button" onClick={() => { setModalShow(true); setSelectedRow(validator) }} className='btn warning-btn light-text w-100'><span>Delegate</span></button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className='box-body'>
-                                                <div className='d-flex align-items-center justify-content-between'>
-                                                    <div className='fw-600 ft-16'>Performance</div>
-                                                    <div>
-                                                        <span className='warning-color fw-600 ft-14'>100%</span>
-                                                    </div>
-                                                </div>
-                                                <div className='d-flex align-items-center justify-content-between'>
-                                                    <div className='fw-600 ft-16'>Commission</div>
-                                                    <div>
-                                                        <span className='warning-color fw-600 ft-14'>{validator.commissionRate}%</span>
-                                                    </div>
-                                                </div>
-                                                <div className='text-center mt-3'>
-                                                    <button type="button" onClick={() => {setModalShow(true);setSelectedRow(validator)}} className='btn warning-btn  light-text w-100'><span>Delegate</span></button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
+                                        </>
+                                    )
+                                })}
 
-                        </div>
+                            </div>
+                            :
+                            <div style={{display:'flex',justifyContent:'center',padding: '3rem'}}>No Records Found.</div>
+                        }
                     </div>
                 </div>
             </div>
