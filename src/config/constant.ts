dab05729c62d572c73d10db57026a76dfadbfabf
rupesh.verma <rupeshkhewar@gmail.com>
2022-07-06 18:09:30 +0530

    network overview page api integration

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,6 +1,11 @@
 export const BONE_ID = 'bone-shibaswap';
 export const STAKE_MANAGER = '0x92f1fe54C01eE8af31f55D400f0e48B7f990F8a2';
 
+export const API_BASE_URL = 'http://18.219.186.181:5020/'
+/**
+ * Staking APi BASE URL
+ */
+export const STAKING_API_BASE_URL = 'http://3.17.75.111:3000/'
 
 /**
  *Using for Transaction via Metamask
