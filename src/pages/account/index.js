7a7d32b85ea9af6040ee73bfbcdf29e03c705817
vivekananad <vivekanand@eiysys.in>
2022-08-22 18:00:50 +0530

    changes updated

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -163,7 +163,7 @@ export default function Account() {
             <div className="baner-card mt-0">
               <h3 class="mb-0 mb-3 text-white fwb">Your Delegations</h3>
               <div className="row">
-                <div className="col-lg-6 col-md-6 col-12 mx-auto bs-col">
+                <div className="col-lg-4 col-md-6 col-12 mx-auto bs-col">
                     <div className="border-sec">
                       <div className="top-sec">
                         <div className="info-block">
@@ -200,10 +200,86 @@ export default function Account() {
                             </div>
                           </div>
                         </div>
+                        <span className="btn card-green text-center d-block btn-small mute-text mt-3">O BONE is in Unbond Period</span>
+                        <ul className="btn-grp">
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn white-btn mute-text btn-small">Restake Reward</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn white-btn mute-text btn-small">Move Stake</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Unbound</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Withdraw Reward</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Stake More</a>
+                            </li>
+                        </ul>
                       </div>
                     </div>
                 </div>
-               <div className="col-lg-6 col-md-6 col-12 mx-auto bs-col">
+                <div className="col-lg-4 col-md-6 col-12 mx-auto bs-col">
+                    <div className="border-sec">
+                      <div className="top-sec">
+                        <div className="info-block">
+                          <div className="image-blk">
+                            <div>
+                              <img className="img-fluid" src="../../assets/images/coin-matic.png" width="69" height="70" alt="coin-icon"/>
+                            </div>
+                          </div>
+                          <div className="grid-info">
+                            <div className="fw-bold">DefiMatic</div>
+                            <div className="info-row">
+                              <span><span className="fw-bold">100%</span> Checkpoints Signed</span>
+                            </div>
+                            <div className="info-row">
+                              <span><span className="fw-bold">1%</span> Commission</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mid-sec bs-card h-auto">
+                        <div className="block-container">
+                          <div className="cus-width"> 
+                            <div className="text-center">
+                              <div>Your Stake</div>
+                              <div className="fw-bold">10</div>
+                              <div>$0</div>
+                            </div>
+                          </div>
+                          <div className="cus-width">
+                            <div className="text-center">
+                              <div>Reward</div>
+                              <div className="fw-bold orange-color">0.04</div>
+                              <div>$0</div>
+                            </div>
+                          </div>
+                        </div>
+                        <span className="btn card-green text-center d-block btn-small mute-text mt-3">O BONE is in Unbond Period</span>
+                        <ul className="btn-grp">
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn white-btn mute-text btn-small">Restake Reward</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn white-btn mute-text btn-small">Move Stake</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Unbound</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Withdraw Reward</a>
+                            </li>
+                            <li className="btn-grp-lst">
+                              <a href="javascript(0)" className="btn btn-primary-outline btn-small">Stake More</a>
+                            </li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+               <div className="col-lg-4 col-md-6 col-12 mx-auto bs-col">
                 <div className="border-sec">
                   <div className="add-sec">
                     <div className="text-center">
