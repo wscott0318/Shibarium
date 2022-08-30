60ab8a4c2ce7a3801c6ea91e41f35c69703a1f0f
Nikki <nikki.panchal@eiysys.in>
2022-08-30 12:48:44 +0530

    added

--- src/pages/components/WalletBalance.tsx
+++ src/pages/components/WalletBalance.tsx
@@ -191,15 +191,6 @@ const WalletBalance = ({ balance, boneUSDValue , getCardsData}: WalletBalancePro
             : <ValidatorAccount getCardsData={getCardsData} balance={balance} boneUSDValue={boneUSDValue} userType={userType} /> 
              
         }
-        <div className="btn-group">
-          <a href="javacript:void(0)" className="btn btn bordered-btn light-text">Restake</a>
-        </div>
-        <div className="btn-group">
-          <a href="javacript:void(0)" className="btn btn bordered-btn light-text">Withdraw Rewards</a>
-        </div>
-        <div className="btn-group">
-          <a href="javacript:void(0)" className="btn btn bordered-btn light-text">Unbond</a>
-        </div>
       </div>
 
     </>
