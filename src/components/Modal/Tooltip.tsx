fbbb83dde2c6a1399785b0045451f77cf8e91c76
Nikki <nikki.panchal@eiysys.in>
2022-09-08 10:50:01 +0530

    added

--- src/components/Modal/Tooltip.tsx
+++ src/components/Modal/Tooltip.tsx
@@ -3,7 +3,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 export default function ArrowTooltips({text, tooltipText}: {text: any, tooltipText: any}) {
   return (
-    <Tooltip placement="right-start" title={tooltipText}arrow>
+    <Tooltip className='position-relative' placement="right-start" title={tooltipText} arrow>
       <p>{text}</p>
     </Tooltip>
   );
--- src/pages/account/Validator.tsx
+++ src/pages/account/Validator.tsx
@@ -571,12 +571,16 @@ const ValidatorAccount = ({ balance, boneUSDValue, userType, getCardsData }: Wal
            
               <form onSubmit={retakeFormikDelegator.handleSubmit} className="modal-form">
                 <div className="form-group">
-                  <label htmlFor="" className="form-label">
+                   <ArrowTooltips 
+                    text={`Validator Address `}
+                    tooltipText="Use Validators Staking Address"
+                    />
+                  {/* <label htmlFor="" className="form-label">
                   Validator Address 
                     <span className="address_tooltip">?
-                    <span className="dummypopup"> Use Validators Staking Address</span>
+                    <span className="dummypopup"> Use Validators Staking Address test</span>
                     </span>
-                  </label>
+                  </label> */}
                   
                   <Field
                     type="text"
