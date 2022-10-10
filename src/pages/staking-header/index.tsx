186fc5bf5d10fb3862a9e16cadf30c07a1d65142
Kushagra Saini <sainikush10@gmail.com>
2022-10-10 15:01:11 +0530

    Fixed Navigation in BoneStaking & tabs in Transactions page

--- src/pages/staking-header/index.tsx
+++ src/pages/staking-header/index.tsx
@@ -1,29 +1,63 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Dropdown, Navbar, Container, Nav } from "react-bootstrap";
 import Link from "next/link";
+import Router, { useRouter } from "next/router";
 
 const StakingHeader = () => {
-
+const router = useRouter();
+const routeHandler = (x:string) => {
+    router.push(x);
+}
+const routeCheck = (x:string) => {
+    return router.asPath.split("/")[1] === x;
+}
     return (
-        <>
-            <div className="staking-header dark-bg-800">
-                <div className="container">
-                    <div className="lft-sec">
-                        <ul className="lft-links ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="javascript:void(0);">Overview</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="javascript:void(0);">All Validators</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="javascript:void(0);">My Account</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+      <>
+        <div className="staking-header dark-bg-800">
+          <div className="container">
+            <div className="lft-sec">
+              <ul className="lft-links ms-auto">
+                <li className="nav-item">
+                  <button onClick={() => routeHandler("/bone-staking")}>
+                    <a
+                      className={`nav-link ${
+                        routeCheck("bone-staking") && "active"
+                      }`}
+                      href="javascript:void(0);"
+                    >
+                      Overview
+                    </a>
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button onClick={() => routeHandler("/all-validator")}>
+                    <a
+                      className={`nav-link ${
+                        routeCheck("all-validator") && "active"
+                      }`}
+                      href="javascript:void(0);"
+                    >
+                      All Validators
+                    </a>
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button onClick={() => routeHandler("/my-account")}>
+                    <a
+                      className={`nav-link ${
+                        routeCheck("my-account") && "active"
+                      }`}
+                      href="javascript:void(0);"
+                    >
+                      My Account
+                    </a>
+                  </button>
+                </li>
+              </ul>
             </div>
-        </>
+          </div>
+        </div>
+      </>
     );
 };
 
