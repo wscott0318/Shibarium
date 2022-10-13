940849c709d5d82dc9e43b5f688b36a11dabeeb8
Kushagra Saini <sainikush10@gmail.com>
2022-10-13 17:52:29 +0530

    WOrked on Delegate Popup in Bone Staking

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -90,8 +90,6 @@ export default function Wallet() {
   }
 
   const getTokensList = () => {
-    setTokenFilteredList([])
-    setTokenList([])
     getWalletTokenList('pos').then(res => {
       let list = res.data.data.tokenList
       list.forEach(async (x: any) => {
@@ -124,7 +122,7 @@ export default function Wallet() {
       getTokensList()
     }
 
-  }, [account, chainId])
+  }, [account])
 
 
 
@@ -331,7 +329,7 @@ export default function Wallet() {
                     <p className="mb-0">Sending funds to exchanges:</p>
                     <div className="exchng_msg_box">
                       <p>Exchanges supported from Shibarium network</p>
-                      <p className="sprdt_txt">Supported Exchanges</p>
+                      <p className="sprdt_txt">Supported Excanges</p>
                     </div>
                     <p className="alert_msg">
                       <div className="image-wrap d-inline-block me-2">
@@ -589,7 +587,7 @@ export default function Wallet() {
                           htmlFor="flexCheckChecked"
                         >
                           I’m not sending funds to an{" "}
-                          <a href="#">unsupported exchange</a> or incorrect
+                          <a href="#">unsupported excange</a> or incorrect
                           address
                         </label>
                       </div>
