0a8005b93096496af9f470b863aed6f42aff8469
nikkipanchal <nikki@revinfotech.com>
2022-10-18 10:35:52 +0530

    added

--- src/components/Shimmer/DynamicShimmer.tsx
+++ src/components/Shimmer/DynamicShimmer.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+// @ts-ignore
 import { ShimmerTable } from "react-shimmer-effects";
 
 function DynamicShimmer({type,rows,cols}:any) {
   if(type === "table")
   {
     return <ShimmerTable row={rows} col={cols} />;
+  } else {
+    return (
+    null
+    )
   }
 }
 
