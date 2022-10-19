f1070ff0e0db4fcebfd60f5282d36aa4664f67ff
abhishek <abhishek.kapoor@eiysys.in>
2022-10-19 18:52:54 +0530

    design enhancements

--- src/pages/all-validator/index.tsx
+++ src/pages/all-validator/index.tsx
@@ -102,7 +102,7 @@ export const Allvalidator: React.FC = () => {
         </section>
         {/* banner section closed */}
 
-        <div ref={myRef} className="container ffms-inherit vivek">
+        <div ref={myRef} className="ffms-inherit vivek">
            <Valitotors withStatusFilter={true} />
           </div>
       </div>
