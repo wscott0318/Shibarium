9b45ed354a1314b95cb199cf04578735338d4b98
Kushagra Saini <sainikush10@gmail.com>
2022-11-04 15:33:37 +0530

    404 page work

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import Header from "pages/layout/header";
+import StakingHeader from "pages/staking-header";
+import Link from "next/link";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <main className="main-content val_account_outr cmn-input-bg dark-bg-800 full-vh top-space ffms-inherit">
+        <Header />
+        <div className="error-container">
+          <div className="text-center">
+            {/* <h1 className="mb-3">
+                            <span className="fade-in" id="digit1">4</span>
+                            <span className="fade-in" id="digit2">0</span>
+                            <span className="fade-in" id="digit3">4</span>
+                        </h1>
+                        <h3 className="fadeIn mb-3">Page  Not Found</h3> */}
+            <img
+              className="img-fluid"
+              src="../../images/error-img.gif"
+              alt="404-error"
+              width={600}
+            />
+            <Link href="/" passHref>
+              <a className="btn primary-btn">Return To Home</a>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
