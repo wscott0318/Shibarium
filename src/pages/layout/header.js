a904522d288847ca395c84bae9a06d813dbab7d2
vivekananad <vivekanand@eiysys.in>
2022-11-07 11:10:29 +0530

    changes updated

--- src/pages/layout/header.js
+++ src/pages/layout/header.js
@@ -163,7 +163,7 @@ const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 1);
+      setScroll(window.scrollY > 60);
     });
   },[])
 
