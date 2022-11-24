bf66784151bb560e816ed1cab9a259fdcab7a8b8
Navdeep Garg <navdeep@revinfotech.com>
2022-11-24 20:06:27 +0530

    .

--- src/pages/all-validator/[id].tsx
+++ src/pages/all-validator/[id].tsx
@@ -37,7 +37,7 @@ export default function ValidatorDetails() {
             const { id } = router.query;
             if (id) {
                 setLoading(true);
-                getValidatorsDetail(id.toString()).then((res) => {
+                getValidatorsDetail(id.toString()).then((res:any) => {
                     setValidatorInfo(res?.data?.data?.validatorSet.validatorInfo)
                     setAllDelegators(res?.data?.data?.validatorSet?.delegators || []);
                     setAllCheckpoints(res?.data?.data?.validatorSet?.checkpoints || [])
