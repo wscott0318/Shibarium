b6ef60469d6a6488f0a68e7471fe5607ecd9fba2
ZaibaShaikh <96591747+ZaibaShaikh@users.noreply.github.com>
2022-11-29 15:53:27 +0530

    optimisez code

--- src/hooks/useSearchFilter.ts
+++ src/hooks/useSearchFilter.ts
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
-import * as Sentry from '@sentry/nextjs';
-
+import * as Sentry from "@sentry/nextjs";
 
 export const useSearchFilter = (data: any[], keyword: string) => {
   const [result, setResult] = useState<any[]>([]);
   useEffect(() => {
     try {
       const filtered = data.filter((name: any) => {
-          if (keyword === "") {
-            return Object.values(name)
-              .join(" ")
-              .toLowerCase()
-              .includes(keyword.toLowerCase());
-          } else if(name.name !== null ){
-            return name.name.toLowerCase().includes(keyword.toLowerCase());
-          }
-        });
-        setResult(filtered);
-    } catch (err : any) {
-      Sentry.captureMessage("useSearchFilter" , err);
-    } 
+        // if (keyword === "") {
+        //   return Object.values(name)
+        //     .join(" ")
+        //     .toLowerCase()
+        //     .includes(keyword.toLowerCase());
+        // } else
+        if (name.name && keyword) {
+          return name.name.toLowerCase().includes(keyword.toLowerCase());
+        } else {
+          return Object.values(name)
+            .join(" ")
+            .toLowerCase()
+            .includes(keyword.toLowerCase());
+        }
+      });
+      setResult(filtered);
+    } catch (err: any) {
+      Sentry.captureMessage("useSearchFilter", err);
+    }
   }, [data, keyword]);
 
   return result;
