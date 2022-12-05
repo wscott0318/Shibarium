63b6b7c9dcd6a3cf9d9ee580002d256d96e0d9f7
abhishek <abhishek.kapoor@eiysys.in>
2022-12-05 13:13:42 +0530

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,7 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
 
         {/* <link rel="manifest" href="/manifest.json" /> */}
-        {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+        <link rel="shortcut icon" href="../../assets/images/favicon.ico" />
       </Head>
       <body>
         <Main />
