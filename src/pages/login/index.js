1b042ee60266bb96f5280f54a7e76fe8dc8dae4b
ZaibaShaikh <96591747+ZaibaShaikh@users.noreply.github.com>
2022-12-06 23:01:22 +0530

    metamask download opening in new tab

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -11,25 +11,24 @@ import Popup from "../components/PopUp";
 // import Web3 from "web3";
 import {
   NoEthereumProviderError,
-  UserRejectedRequestError as UserRejectedRequestErrorInjected
-} from '@web3-react/injected-connector'
-
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from "@web3-react/injected-connector";
+import Link from "next/link";
 import Web3Status from "app/components/Web3Status";
 import { useActiveWeb3React } from "app/services/web3";
 
 export default function Login() {
-  const router = useRouter()
+  const router = useRouter();
   const [showInsModal, setShowInsModal] = useState(false);
- 
-  const { account } = useActiveWeb3React()
+
+  const { account } = useActiveWeb3React();
   // const account = useAccount()
-   
-  const connectToMetamask=()=>{
+
+  const connectToMetamask = () => {
     // authenticate()
     // activate(walletConnector)
-  }
+  };
 
-  
   // useEffect(() => {
   //   if (library) {
   //    let web3 =  new Web3(library?.provider)
@@ -43,7 +42,6 @@ export default function Login() {
   //    }
   //   }
   // }, [library,account])
-  
 
   // useEffect(()=>{
   //  const isLoggedIn =  localStorage.getItem('isLoggedIn')
@@ -52,59 +50,73 @@ export default function Login() {
   //  }
   // },[])
   useEffect(() => {
-    if(account){
+    if (account) {
       // handleAccount(account)
-      router.push('/home')
+      router.push("/home");
     }
-    },[account]);
-    // useEffect(() => {
-    //   // if(error){
-    //   //   const errorMsg = getErrorMessage(error)
-    //   //  alert(errorMsg)
-    //   //  }
-    // },[error]);
-  
-    function getErrorMessage(error) {
-      if (error instanceof NoEthereumProviderError) {
-        return 'Please install metamask and try again.'
-      } else if (error instanceof UnsupportedChainIdError) {
-        return "You're connected to an unsupported network."
-      } else if (
-        error instanceof UserRejectedRequestErrorInjected ||
-        error instanceof UserRejectedRequestErrorWalletConnect
-      ) {
-        return 'Please authorize this website to access your Ethereum account.'
-      } 
-      else {
-        console.error(error)
-        return ''
-      }
+  }, [account]);
+  // useEffect(() => {
+  //   // if(error){
+  //   //   const errorMsg = getErrorMessage(error)
+  //   //  alert(errorMsg)
+  //   //  }
+  // },[error]);
+
+  function getErrorMessage(error) {
+    if (error instanceof NoEthereumProviderError) {
+      return "Please install metamask and try again.";
+    } else if (error instanceof UnsupportedChainIdError) {
+      return "You're connected to an unsupported network.";
+    } else if (
+      error instanceof UserRejectedRequestErrorInjected ||
+      error instanceof UserRejectedRequestErrorWalletConnect
+    ) {
+      return "Please authorize this website to access your Ethereum account.";
+    } else {
+      console.error(error);
+      return "";
     }
-    
+  }
 
   return (
-   <>
+    <>
       <main className="main-content full-vh dark-bg-800">
         {/* login section start */}
-        <p className="mb-0" style={{display:'flex', justifyContent:'center'}}>
-        </p>
+        <p
+          className="mb-0"
+          style={{ display: "flex", justifyContent: "center" }}
+        ></p>
         <section className="login-section">
-            <div className="container">
-                <div className='login_h_logo text-center d-none'>
-                    <a href='#' className=''><img className='img-fluid' src="../../assets/images/logo.png" alt="site-logo" width={250} /></a>
-                </div>
-                <div className='login_holder'>
-                    <div className='login_outr'>
-                        <h2>Login</h2>
-                         <Web3Status />
-                       
-                        <p className='sign_up_optn'>Don’t have a wallet? <a target="_blank" href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">Download here</a></p>
-                    </div>
-                </div>
+          <div className="container">
+            <div className="login_h_logo text-center d-none">
+              <a href="#" className="">
+                <img
+                  className="img-fluid"
+                  src="../../assets/images/logo.png"
+                  alt="site-logo"
+                  width={250}
+                />
+              </a>
             </div>
-        </section> 
+            <div className="login_holder">
+              <div className="login_outr">
+                <h2>Login</h2>
+                <Web3Status />
+                <p className="sign_up_optn">
+                  Don’t have a wallet?{" "}
+                  <Link
+                    href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
+                    passHref
+                  >
+                    <a target="_blank">Download here</a>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
         {/* login section end */}
       </main>
-   </>
+    </>
   );
 }
