64b238aa854ceb43f9ce091e06d9b9c70722c9a7
nikkipanchal <nikki@revinfotech.com>
2022-12-08 17:22:28 +0530

    added

--- src/services/apis/user/userApi.ts
+++ src/services/apis/user/userApi.ts
@@ -7,6 +7,7 @@ export function getUserType(address:string){
 export function getDelegatorData(address:string){
     return http.get(`delegators/overview/${address}`)
 }
+
 export function setDelegatorData(address:string, id:any){
     return http.post(`delegators/delegatorinfo?address=${address}&validatorId=${id}`)
 }
