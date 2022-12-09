8378460b9f3e36be2cba5c18da54d9b678fb354d
wscott <wscott0318@gmail.com>
2022-12-09 11:29:33 +0530

    Added toggle on staking header

--- src/pages/staking-header/index.tsx
+++ src/pages/staking-header/index.tsx
@@ -31,21 +31,21 @@ const StakingHeader = () => {
   const [valId, setValId] = useValId();
   const [valInfoModal, setValInfoModal] = useState(false);
   const [dynasty, setDynasty] = useState('')
-  const { account, chainId = 1, library,active } = useActiveWeb3React();
+  const { account, chainId = 1, library, active } = useActiveWeb3React();
 
   const [valInfo, setValInfo] = useValInfo();
   const dispatch = useAppDispatch();
- useEffect(() => {
-   const { ethereum } = window as any;
-   const handleAccountsChanged = (accounts: string[]) => {
-     console.log("Handling 'accountsChanged' event with payload", accounts);
-     setValInfoModal(false);
-     dispatch(clearAllTransactions({ chainId }));
-   };
+  useEffect(() => {
+    const { ethereum } = window as any;
+    const handleAccountsChanged = (accounts: string[]) => {
+      console.log("Handling 'accountsChanged' event with payload", accounts);
+      setValInfoModal(false);
+      dispatch(clearAllTransactions({ chainId }));
+    };
 
-   ethereum.on("accountsChanged", handleAccountsChanged);
- }, [active]);
-
+    ethereum.on("accountsChanged", handleAccountsChanged);
+  }, [active]);
+  const [stakingHeader, showStakingHeader] = useState(false);
   const getValInfoApi = async (id: any) => {
     try {
       await getValidatorInfo(id).then(res => {
@@ -55,7 +55,7 @@ const StakingHeader = () => {
           console.log("val ID = ", valId);
           setValInfo(info);
         }
-      }).catch(err => console.log("err => " , err))
+      }).catch(err => console.log("err => ", err))
     } catch (error: any) {
       // console.log("catch err => ", error);
       Sentry.captureMessage("getValInfoApi ", error);
@@ -78,9 +78,9 @@ const StakingHeader = () => {
     } else if (routeCheck("reward-history")) {
       setHistory("Reward History");
     }
-    if(account){
+    if (account) {
       getDynsetyValue()
-    } 
+    }
   }, [router, account]);
 
   // console.log("usertype ==> ", valId);
@@ -95,7 +95,7 @@ const StakingHeader = () => {
       );
       const dynasty = await instance.methods.dynasty().call({ from: account });
       setDynasty(dynasty)
-    } catch(err: any) {
+    } catch (err: any) {
 
     }
   }
@@ -107,14 +107,15 @@ const StakingHeader = () => {
           return (
             <>
               <li className="nav-item">
-                <p className="nav-link ff-mos" onClick={() => setValInfoModal(true)}>
-                  <p
+                <p className={`nav-link ff-mos ${router.asPath === "/my-account" ? "active" : ""
+                  }`} onClick={() => setValInfoModal(true)}>
+                  {/* <p
                     className={`nav-link ff-mos ${
                       router.asPath === "/my-account" ? "active" : ""
                     }`}
-                  >
-                    My Account
-                  </p>
+                  > */}
+                  My Account
+                  {/* </p> */}
                 </p>
               </li>
               <li className="nav-item">
@@ -136,9 +137,8 @@ const StakingHeader = () => {
               <li className="nav-item">
                 <Link href="/my-account" className="nav-link ff-mos" passHref>
                   <p
-                    className={`nav-link ff-mos ${
-                      router.asPath === "/my-account" ? "active" : ""
-                    }`}
+                    className={`nav-link ff-mos ${router.asPath === "/my-account" ? "active" : ""
+                      }`}
                   >
                     My Account
                   </p>
@@ -162,38 +162,37 @@ const StakingHeader = () => {
         }
       } else if (userType === "Delegator") {
         return (
-        <>
-          <li className="nav-item">
-            <Link href="/my-account" className="nav-link ff-mos" passHref>
-              <p
-                className={`nav-link ff-mos ${
-                  router.asPath === "/my-account" ? "active" : ""
-                }`}
-              >
-                My Account
-              </p>
-            </Link>
-          </li>
+          <>
+            <li className="nav-item">
+              <Link href="/my-account" className="nav-link ff-mos" passHref>
+                <p
+                  className={`nav-link ff-mos ${router.asPath === "/my-account" ? "active" : ""
+                    }`}
+                >
+                  My Account
+                </p>
+              </Link>
+            </li>
 
-          <li className="nav-item">
-            <DropdownButton
-              className="dd-style cus-arw"
-              id="dropdown-item-button"
-              title={"History"}
-            >
-              <Dropdown.Item>
-                <Link href="/unbond-history" passHref>
-                  Unbound History
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <Link href="/reward-history" passHref>
-                  Reward History
-                </Link>
-              </Dropdown.Item>
-            </DropdownButton>
-          </li>
-        </>
+            <li className="nav-item">
+              <DropdownButton
+                className="dd-style cus-arw"
+                id="dropdown-item-button"
+                title={"History"}
+              >
+                <Dropdown.Item>
+                  <Link href="/unbond-history" passHref>
+                    Unbound History
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link href="/reward-history" passHref>
+                    Reward History
+                  </Link>
+                </Dropdown.Item>
+              </DropdownButton>
+            </li>
+          </>
         )
       } else {
         return (
@@ -201,9 +200,8 @@ const StakingHeader = () => {
             <li className="nav-item">
               <Link href="/my-account" className="nav-link ff-mos" passHref>
                 <p
-                  className={`nav-link ff-mos ${
-                    router.asPath === "/my-account" ? "active" : ""
-                  }`}
+                  className={`nav-link ff-mos ${router.asPath === "/my-account" ? "active" : ""
+                    }`}
                 >
                   My Account
                 </p>
@@ -231,43 +229,59 @@ const StakingHeader = () => {
               <div className="dark-bg-800 h-100 status-sec sec-ht position-relative status-sep-popup">
                 <img
                   className="img-fluid"
-                  src="../../assets/images/waiting-small.png" 
+                  src="../../assets/images/waiting-small.png"
                   alt="Wait"
                 />
-               <p className="light-text primary-text"> Wait for {dynasty} checkpoints to see your account info...</p>
+                <p className="light-text primary-text"> Wait for {dynasty} checkpoints to see your account info...</p>
               </div>
             </div>
           </div>
         </div>
       </ValInfoModals>
-      <div className="staking-header dark-bg-800">
-        <div className="container">
-          <div className="lft-sec">
-            <ul className="lft-links ms-auto">
-              <li className="nav-item">
-                <Link href="/bone-staking" passHref>
-                  <p
-                    className={`nav-link ff-mos ${
-                      router.asPath === "/bone-staking" ? "active" : ""
-                    }`}
-                  >
-                    Overview
-                  </p>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/all-validator" passHref>
-                  <p
-                    className={`nav-link ff-mos ${
-                      router.asPath === "/all-validator" ? "active" : ""
-                    }`}
-                  >
-                    All Validators
-                  </p>
-                </Link>
-              </li>
-              {renderButtons()}
-            </ul>
+      <div className="staking_header_wrapper">
+        <div className="staking-header dark-bg-800">
+          <button className="toggle_staking_header" onClick={() => showStakingHeader(!stakingHeader)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+          <div className={`container ${stakingHeader ? "active" : ""}`}>
+            {stakingHeader && <span
+              className="close-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => showStakingHeader(false)}
+            >
+              <img
+                className="img-fluid"
+                src="../../assets/images/close-icon.png"
+                alt="close-icon"
+              />
+            </span>}
+            <div className="lft-sec">
+              <ul className="lft-links ms-auto">
+                <li className="nav-item">
+                  <Link href="/bone-staking" passHref>
+                    <p
+                      className={`nav-link ff-mos ${router.asPath === "/bone-staking" ? "active" : ""
+                        }`}
+                    >
+                      Overview
+                    </p>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/all-validator" passHref>
+                    <p
+                      className={`nav-link ff-mos ${router.asPath === "/all-validator" ? "active" : ""
+                        }`}
+                    >
+                      All Validators
+                    </p>
+                  </Link>
+                </li>
+                {renderButtons()}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
