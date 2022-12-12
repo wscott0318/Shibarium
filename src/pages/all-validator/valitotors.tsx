0ed39387955e522125039f1f79f4d4694edf668d
wscott <wscott0318@gmail.com>
2022-12-12 10:45:23 +0530

    added select option on bridge page deposit and withdraw form, resolved jria ticket 1704

--- src/pages/all-validator/valitotors.tsx
+++ src/pages/all-validator/valitotors.tsx
@@ -103,7 +103,7 @@ const Valitotors: React.FC<any> = ({ withStatusFilter }: { withStatusFilter: boo
   useEffect(() => {
     let filtered = []
     if (isActiveTab) {
-      filtered = allValidators.filter(e => e.uptimePercent >= inActiveCount)
+      filtered = allValidators.filter(e => e.uptimePercent >= inActiveCount && e.fundamental == 2)
     } else {
       filtered = allValidators.filter(e => e.uptimePercent <= inActiveCount)
     }
