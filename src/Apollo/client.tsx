c690018d822097040e2177dbf953c0c609926581
Robil <robil@eiysys.in>
2023-01-06 18:15:31 +0530

    .

--- src/Apollo/client.tsx
+++ src/Apollo/client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 
 export const queryProvider = new ApolloClient({
     link: createHttpLink({
-        uri: 'https://api.studio.thegraph.com/query/33694/shibarium-v3/v0.0.11'
+        uri: 'https://api.studio.thegraph.com/query/33694/shibarium-517/v0.0.2'
     }),
     cache: new InMemoryCache()
 })
