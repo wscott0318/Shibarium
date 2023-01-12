23cd1a6592d5380b50ca4b3205626b47ffc5a86d
nikkipanchal <nikki@revinfotech.com>
2023-01-12 09:47:22 +0530

    alchamy key changed for gorli test net

--- src/config/rpc.ts
+++ src/config/rpc.ts
@@ -8,7 +8,7 @@ const RPC = {
   // [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
   [ChainId.ROPSTEN]: 'https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW',
   [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
-  [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
+  [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/MOMfwk1FvOZhStP8c0Z9CHep6Rvi0Z95',
   [ChainId.KOVAN]: 'https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER',
   [ChainId.FANTOM]: 'https://rpcapi.fantom.network',
   [ChainId.FANTOM_TESTNET]: 'https://rpc.testnet.fantom.network',
