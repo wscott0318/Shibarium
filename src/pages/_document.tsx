53d6242ed1e45c92737b956af570d62625c9aa59
wscott <wscott0318@gmail.com>
2023-03-05 20:07:39 +0530

    added gtm code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,13 +9,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S2TGREN362"></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KG67G9K');`,
+            __html: ` window.dataLayer = window.dataLayer || [];
+            {/* @ts-ignore */}
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S2TGREN362');`,
           }}
         />
         <meta name="application-name" content={APP_NAME} />
@@ -42,7 +44,9 @@ export default function Document() {
 
       </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
         <GTMBody />
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
