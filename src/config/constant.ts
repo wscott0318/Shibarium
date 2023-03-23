15eeba4c91c1e4ced0835320c54e18ddc8058dd2
navdeepgarg1 <robil@eiysys.in>
2023-03-23 15:05:38 +0530

    .

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -3,7 +3,7 @@ import { ChainId } from "shibarium-get-chains";
 export const BONE_ID = 'bone-shibaswap';
 // 18.216.5.132:3000/api/v1/ http://18.216.5.132:3000/api/v1/tokens/getList
                                 
-export const API_BASE_URL = 'https://preprod-api.shib.io/api/v1' // dev
+export const API_BASE_URL = 'https://preprod-api1.shib.io/api/v1' // dev
 // export const API_BASE_URL = 'https://staging-api.hailshiba.com/api/v1' // staging
 
 /**
