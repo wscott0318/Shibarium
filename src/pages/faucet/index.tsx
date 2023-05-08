4b3bca4439fb3d38f071de0fe2a86503a82f7663
wscott <wscott0318@gmail.com>
2023-05-08 09:40:20 +0530

    added faucet new api

--- src/pages/faucet/index.tsx
+++ src/pages/faucet/index.tsx
@@ -3,27 +3,32 @@
 import React, { useState, useEffect, useRef } from "react";
 import CommonModal from "../components/CommonModel";
 import InnerHeader from "../../pages/inner-header";
-import Sidebar from "../layout/sidebar"
+import Sidebar from "../layout/sidebar";
 import axios from "axios";
 import { useActiveWeb3React } from "app/services/web3";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useRouter } from "next/router";
-import { CircularProgress, FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
+import {
+  CircularProgress,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
 import { getExplorerLink } from "app/functions";
 import { ChainId } from "shibarium-get-chains";
-import * as Sentry from '@sentry/nextjs';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import * as Sentry from "@sentry/nextjs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function Faucet() {
   const [showSwapModal, setSwapModal] = useState(false);
   const [menuState, setMenuState] = useState(false);
   const router = useRouter();
-  const siteKey:any = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+  const siteKey: any = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
   const [selectedChain, setSelectedChain] = useState(1);
   const [modalState, setModalState] = useState({
     pending: true,
     done: false,
-    hash: ''
+    hash: "",
   });
   const recaptchaRef = useRef<ReCAPTCHA>(null);
   const [isActive, setIsActive] = useState(1);
@@ -31,72 +36,119 @@ export default function Faucet() {
 
   const handleMenuState = () => {
     setMenuState(!menuState);
-  }
+  };
   const handleChange = (e: any) => {
     setSelectedChain(e.target.value);
-    console.log("value inside e", e.target.value)
+    console.log("value inside e", e.target.value);
     setIsActive(e.target.value);
-  }
+  };
   useEffect(() => {
     if (!account) {
-      router.back()
+      router.back();
     }
-  }, [account])
+  }, [account]);
 
   const callFaucetAPI = async () => {
-    setSwapModal(true)
+    setSwapModal(true);
     setModalState({
       pending: true,
       done: false,
-      hash: ''
-    })
-    console.log("entered faucet api" ,recaptchaRef);
+      hash: "",
+    });
+    console.log("entered faucet api", account);
     try {
-      await axios.get(`https://faucet.shib.io/api/faucet/${account}?type=${selectedChain}`)
-        .then((res: any) => {
-          recaptchaRef.current?.reset()
+      await axios
+        .post(`http://10.89.6.101:5000/api/getToken`, {
+          address: account,
+        })
+        .then(async (res: any) => {
+          recaptchaRef.current?.reset();
+          let accessToken = res.data.data.token;
+          console.log("access token ", res);
+          const headers = {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          };
+          await axios
+            .post(
+              `http://10.89.6.101:5000/api/getAuthToken`,
+              {},
+              {
+                headers: headers,
+              }
+            )
+            .then(async (res: any) => {
+              await axios
+                .post(
+                  `http://10.89.6.101:5000/api/faucet`,
+                  {
+                    type: selectedChain,
+                  },
+                  { headers: headers }
+                )
+                .then((res: any) => {
+                  setModalState({
+                    pending: false,
+                    done: true,
+                    hash: res.data.data.transactionHash,
+                  });
+                  toast.success("Faucet claimed! Check wallet.", {
+                    position: toast.POSITION.TOP_RIGHT,
+                    autoClose: 3000,
+                    type: toast.TYPE.SUCCESS,
+                  });
+                })
+                .catch((err: any) => {
+                  console.log(JSON.stringify(err.message));
+                  if (err.message === "Request failed with status code 409") {
+                    toast.error("Faucet can be claimed once every 24 hours.", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      autoClose: 5000,
+                    });
+                  } else {
+                    toast.error("Something went wrong", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      autoClose: 5000,
+                    });
+                  }
+                  setSwapModal(false);
+                  // console.log("err =>", err)
+                  setClickedCaptcha(false);
+                  recaptchaRef.current?.reset();
+                });
+            });
           // console.log("response ", res);
-          setModalState({
-            pending: false,
-            done: true,
-            hash: res.data.data.transactionHash
-          })
-          toast.success('Faucet claimed! Check wallet.', {
-            position: toast.POSITION.TOP_RIGHT, autoClose: 3000,
-            type:toast.TYPE.SUCCESS
-          });
-        }).catch((err: any) => {
+        })
+        .catch((err: any) => {
           console.log(JSON.stringify(err.message));
-          if(err.message==="Request failed with status code 409")
-          {
+          if (err.message === "Request failed with status code 409") {
             toast.error("Faucet can be claimed once every 24 hours.", {
-              position: toast.POSITION.TOP_RIGHT, autoClose: 5000
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 5000,
             });
-          }
-          else{
+          } else {
             toast.error("Something went wrong", {
-              position: toast.POSITION.TOP_RIGHT, autoClose: 5000
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 5000,
             });
           }
-          setSwapModal(false)
+          setSwapModal(false);
           // console.log("err =>", err)
           setClickedCaptcha(false);
-          recaptchaRef.current?.reset()
-        })
+          recaptchaRef.current?.reset();
+        });
     } catch (err: any) {
       Sentry.captureMessage("callFaucetAPI" + selectedChain, err);
-      console.log("error-> ", err)
+      console.log("error-> ", err);
     }
+  };
 
-  }
-
-  const [clickedCaptcha, setClickedCaptcha] = useState(false)
+  const [clickedCaptcha, setClickedCaptcha] = useState(false);
 
   const handleCaptcha = (e: any) => {
-
     // console.log("receptcha event ", e)
     setClickedCaptcha(true);
-  }
+  };
   const imageOnErrorHandler = (
     event: React.SyntheticEvent<HTMLImageElement, Event>
   ) => {
@@ -107,18 +159,20 @@ export default function Faucet() {
   const handleExplorer = () => {
     let link: any;
     if (selectedChain == 1) {
-      link = getExplorerLink(ChainId.GÖRLI, modalState?.hash, "transaction")
+      link = getExplorerLink(ChainId.GÖRLI, modalState?.hash, "transaction");
+    } else {
+      link = getExplorerLink(
+        ChainId.PUPPYNET719,
+        modalState?.hash,
+        "transaction"
+      );
     }
-    else {
-      link = getExplorerLink(ChainId.PUPPYNET719, modalState?.hash, "transaction")
-    }
-    window.open(link)
-  }
+    window.open(link);
+  };
   return (
     <>
       <ToastContainer />
       <main className="main-content">
-
         <Sidebar
           handleMenuState={handleMenuState}
           onClickOutside={() => {
@@ -130,14 +184,15 @@ export default function Faucet() {
         <div className="cmn_dashbord_main_outr">
           <InnerHeader />
 
-          <div className='swap-card cus-card-800'>
+          <div className="swap-card cus-card-800">
             <div className="swp-header">
-              <div className='mb-3 swp-left-col mb-lg-3 mb-xl-4'>
+              <div className="mb-3 swp-left-col mb-lg-3 mb-xl-4">
                 <h2 className="mb-4">Faucet</h2>
-                <h6 className='mb-2'>
-                  Get Gas Coin
-                </h6>
-                <p className='grey-txt'>This faucet transfers Gas Coin on Shibarium testnet. Confirm details before submitting.</p>
+                <h6 className="mb-2">Get Gas Coin</h6>
+                <p className="grey-txt">
+                  This faucet transfers Gas Coin on Shibarium testnet. Confirm
+                  details before submitting.
+                </p>
               </div>
             </div>
             <div className="fau_tabs_area">
@@ -180,14 +235,48 @@ export default function Faucet() {
                                 className="radioGroup"
                                 onChange={handleChange}
                               >
-                                <FormControlLabel value="1" control={<Radio />}
-                                  label={<div className="d-flex justify-content-center align-items-center" style={{ height: "34px" }}>
-                                    <img width={18} src="../../assets/images/eth.png" className="me-2" onError={imageOnErrorHandler} /> Goerli BONE</div>}
-                                  className={`radioButtons ${isActive == 1 && "active"}`} />
-                                <FormControlLabel value="2" control={<Radio />}
-                                  label={<div className="d-flex justify-content-center align-items-center" style={{ height: "34px" }}>
-                                    <img width={24} src="../../assets/images/shib-logo.png" className="me-2" onError={imageOnErrorHandler} /> Puppy Net BONE</div>}
-                                  className={`radioButtons ${isActive == 2 && "active"}`} />
+                                <FormControlLabel
+                                  value="1"
+                                  control={<Radio />}
+                                  label={
+                                    <div
+                                      className="d-flex justify-content-center align-items-center"
+                                      style={{ height: "34px" }}
+                                    >
+                                      <img
+                                        width={18}
+                                        src="../../assets/images/eth.png"
+                                        className="me-2"
+                                        onError={imageOnErrorHandler}
+                                      />{" "}
+                                      Goerli BONE
+                                    </div>
+                                  }
+                                  className={`radioButtons ${
+                                    isActive == 1 && "active"
+                                  }`}
+                                />
+                                <FormControlLabel
+                                  value="2"
+                                  control={<Radio />}
+                                  label={
+                                    <div
+                                      className="d-flex justify-content-center align-items-center"
+                                      style={{ height: "34px" }}
+                                    >
+                                      <img
+                                        width={24}
+                                        src="../../assets/images/shib-logo.png"
+                                        className="me-2"
+                                        onError={imageOnErrorHandler}
+                                      />{" "}
+                                      Puppy Net BONE
+                                    </div>
+                                  }
+                                  className={`radioButtons ${
+                                    isActive == 2 && "active"
+                                  }`}
+                                />
                               </RadioGroup>
                             </div>
                             {/* </div>
@@ -209,24 +298,32 @@ export default function Faucet() {
                             </div>
                           </div>
 
-                          <div className="mt-3 captcha-wrap mt-sm-4" >
+                          <div className="mt-3 captcha-wrap mt-sm-4">
                             <ReCAPTCHA
                               ref={recaptchaRef}
                               sitekey={siteKey}
-                              asyncScriptOnLoad={() => { }}
+                              asyncScriptOnLoad={() => {}}
                               onChange={handleCaptcha}
-                              onExpired={() => {setClickedCaptcha(false); recaptchaRef.current?.reset()}}
+                              onExpired={() => {
+                                setClickedCaptcha(false);
+                                recaptchaRef.current?.reset();
+                              }}
                             />
                           </div>
                           <div className="mt-3 ">
-                            <button disabled={!clickedCaptcha} onClick={() => callFaucetAPI()} type="button" className="btn primary-btn w-100">Submit</button>
+                            <button
+                              disabled={!clickedCaptcha}
+                              onClick={() => callFaucetAPI()}
+                              type="button"
+                              className="btn primary-btn w-100"
+                            >
+                              Submit
+                            </button>
                           </div>
-
                         </form>
                       </div>
                     </div>
                   </div>
-
                 </div>
               </div>
             </div>
@@ -236,34 +333,48 @@ export default function Faucet() {
       </main>
       {/* Review model code start */}
       <CommonModal
-        title={modalState.pending ? "Transaction Pending" : 'Transaction Submitted'}
+        title={
+          modalState.pending ? "Transaction Pending" : "Transaction Submitted"
+        }
         show={showSwapModal}
         setshow={setSwapModal}
-        externalCls="faucet-pop">
+        externalCls="faucet-pop"
+      >
         <div className="popmodal-body tokn-popup no-ht trans-mod">
           <div className="pop-block">
             <div className="pop-top">
-              <div className='dark-bg-800 h-100 status-sec'>
-                {modalState.pending ?
+              <div className="dark-bg-800 h-100 status-sec">
+                {modalState.pending ? (
                   <span className="p-5">
                     <CircularProgress size={130} style={{ color: "#f27c02" }} />
-                  </span> :
-                  <span className="p-4">
-                    <div><img width="180" height="170" className="img-fluid" src="../../assets/images/Ellipse.png" alt="" onError={imageOnErrorHandler} /></div>
                   </span>
-                }
+                ) : (
+                  <span className="p-4">
+                    <div>
+                      <img
+                        width="180"
+                        height="170"
+                        className="img-fluid"
+                        src="../../assets/images/Ellipse.png"
+                        alt=""
+                        onError={imageOnErrorHandler}
+                      />
+                    </div>
+                  </span>
+                )}
               </div>
             </div>
             <div className="pop-bottom">
               {/* <p className='mt-3 elip-text'>{modalState.hash}</p> */}
-              <div className='staus-btn'>
+              <div className="staus-btn">
                 <button
-                  type='button'
-                  className='btn primary-btn w-100'
+                  type="button"
+                  className="btn primary-btn w-100"
                   disabled={modalState.hash ? false : true}
                   onClick={handleExplorer}
                 >
-                  View on Explorer</button>
+                  View on Explorer
+                </button>
               </div>
             </div>
           </div>
@@ -273,6 +384,4 @@ export default function Faucet() {
       {/* Review model code end */}
     </>
   );
-
-
 }
