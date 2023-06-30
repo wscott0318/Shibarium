46e58818cd3786f37f5e7c8b643326b6373d3442
wscott <wscott0318@gmail.com>
2023-06-30 17:09:26 +0530

    latest

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -3,8 +3,8 @@ import { ChainId } from "shibarium-get-chains";
 export const BONE_ID = "bone-shibaswap";
 // 18.216.5.132:3000/api/v1/ http://18.216.5.132:3000/api/v1/tokens/getList
 
-export const API_BASE_URL = "https://preprod-api.shib.io/api/v1"; // dev
-//export const API_BASE_URL = "http://10.89.4.174:3000/api/v1"; // staging
+// export const API_BASE_URL = "https://preprod-api.shib.io/api/v1"; // dev
+export const API_BASE_URL = "http://10.89.4.174:3000/api/v1"; // staging
 
 /**
  * Staking APi BASE URL
