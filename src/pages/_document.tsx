039d4ef58c22336b4a9334f9f13550e4687f75d4
wscott <wscott0318@gmail.com>
2023-08-17 01:02:25 +0530

    change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 // pages/_document.js
-import { Head, Html, Main, NextScript } from 'next/document'
-const APP_NAME = 'Shibarium Beta'
-const APP_DESCRIPTION = 'Swap, yield, lend, borrow, leverage, limit, launch all on one community driven ecosystem'
+import { Head, Html, Main, NextScript } from "next/document";
+const APP_NAME = "Shibarium";
+const APP_DESCRIPTION =
+  "Swap, yield, lend, borrow, govern, leverage, limit build all on one community driven ecosystem";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S2TGREN362"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-S2TGREN362"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: ` window.dataLayer = window.dataLayer || [];
@@ -27,24 +31,55 @@ export default function Document() {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#0993ec" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="96x96" href="/icons/icon-96x96.png" />
-        <link rel="apple-touch-icon" sizes="128x128" href="/icons/icon-128x128.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
-        <link rel="apple-touch-icon" sizes="384x384" href="/icons/icon-384x384.png" />
-        <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icons/icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="96x96"
+          href="/icons/icon-96x96.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="128x128"
+          href="/icons/icon-128x128.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icons/icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/icons/icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="384x384"
+          href="/icons/icon-384x384.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="/icons/icon-512x512.png"
+        />
 
         {/* <link rel="manifest" href="/manifest.json" /> */}
         <link rel="shortcut icon" href="../../assets/images/favicon.ico" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
